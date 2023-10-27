a9317cda97a93adbbcf50714cb7aa8438aaf1517
aminafarah <amishkamohamed563@gmail.com>
2023-10-27 17:49:01 +0300

    all test passed,eventlab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React,{ useState } from "react";
 
 function Item({ name, category }) {
+
+  let [inCart,setinCart]=useState(false)
+
+  
+  function AddToCart(){
+    setinCart((inCart)=(!inCart))
+  }
   return (
-    <li className="">
+    <li className={inCart? "in-cart" :""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart? "remove":"add"} onClick={AddToCart}>{inCart ? "Remove From Cart": "Add To Cart"} </button>
     </li>
   );
 }
